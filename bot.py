80d709ed65aaea6e891361c5458bb41b07d384ec
Dante <dkirsman@gmail.com>
2024-09-16 23:32:46 -0600

    feat(auto role): give member role when someone submits rule screening

--- bot.py
+++ bot.py
@@ -1,29 +1,51 @@
-from dotenv import load_dotenv
 import os
-import discord
 
-load_dotenv()
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+_ = load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
 
-client = discord.Client(intents=intents)
-
-
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+bot = commands.Bot(command_prefix=";!", intents=intents)
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.event
+async def on_ready() -> None:
+    print(f"We have logged in as {bot.user}")
+
+
+@bot.event
+async def on_member_update(before: discord.Member, after: discord.Member):
+    if before.pending and not after.pending:
+        guild = bot.get_guild(before.guild.id)
+        if not guild:
+            print("Member isn't in a server??")
+            return
+
+        member = guild.get_member(before.id)
+        if not member:
+            print("Member doesn't exist??")
+            return
+
+        role = discord.utils.get(before.guild.roles, name="Member")
+        if not role:
+            print("Member role doesn't exist??")
+            return
+
+        await member.add_roles(role)
+
+
+def main() -> None:
+    if not TOKEN:
+        print("TOKEN doesnt exist??? Check .env file.")
         return
-
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    bot.run(TOKEN)
 
 
-client.run(TOKEN)
+if __name__ == "__main__":
+    main()
